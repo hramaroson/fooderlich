64f45fa57ea7f0e343c2450f02045eb55611acb7
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-10 17:45:16 +0300

    Move Home Page to home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/fooderlich_theme.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,30 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fooderlich',
       theme: theme,
-      home: const MyHomePage(title: 'Fooderlich'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text("Let's go cooking", 
-        style: Theme.of(context).textTheme.headline1)
-      )
+      home: const Home(title: 'Fooderlich'),
     );
   }
 }
