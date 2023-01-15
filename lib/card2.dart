ec08ef305a2b1a3871cd17accc2f461e53ea463e
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-15 20:30:59 +0300

    Add recipe and smoothies Text

--- lib/card2.dart
+++ lib/card2.dart
@@ -8,18 +8,48 @@ class Card2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        constraints: const BoxConstraints.expand(
-          width: 350,
-          height: 450
-        ),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/mag2.png")) 
-        ),
-        child: Column(
-          children: const [],
+              constraints: const BoxConstraints.expand(
+                width: 350,
+                height: 450
+              ),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/mag2.png"),
+                  fit: BoxFit.cover
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0)
+                )
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child:Stack( 
+                      children: [
+                        Positioned(
+                          bottom: 16,
+                          right: 16,
+                          child: Text(
+                            "Recipe",
+                            style: FooderlichTheme.lightTextTheme.headline1,
+                          )
+                        ),
+                        Positioned(
+                          bottom: 70,
+                          left: 16,
+                          child: RotatedBox(
+                            quarterTurns: 3,
+                            child: Text( 
+                              "Smoothies",
+                              style: FooderlichTheme.lightTextTheme.headline1)
+                            )
+                        )
+                      ],
+                    ),
+                  )
+                ]
+              )
         )
-      )
     );
   }
 }
