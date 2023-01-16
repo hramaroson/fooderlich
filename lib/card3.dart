6858ca26d0b77f1b2f79e90911ccc9c81730038f
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-16 23:12:31 +0300

    Composing the header

--- lib/card3.dart
+++ lib/card3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
 
 class Card3 extends StatelessWidget{
 
@@ -26,6 +27,26 @@ class Card3 extends StatelessWidget{
                 color: Colors.black.withOpacity(0.6),
                 borderRadius: const BorderRadius.all(Radius.circular(10.0))
               ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40
+                  ),
+                  const SizedBox(height: 8),
+                  Text( 
+                    "Recipe Trends",
+                    style: FooderlichTheme.darkTextTheme.headline2,
+                  ),
+                  const SizedBox(height: 30),
+                ],
+              ),
+
             )
           ],
          )
