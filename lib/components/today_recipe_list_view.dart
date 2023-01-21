fe98106435bf95c9a344fb8d320d309d725b64ed
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-21 20:21:27 +0300

    Fix imports

--- lib/components/today_recipe_list_view.dart
+++ lib/components/today_recipe_list_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fooderlich/components/components.dart';
+import '../components/components.dart';
 import '../models/models.dart';
 
 class TodayRecipeListView extends StatelessWidget{
