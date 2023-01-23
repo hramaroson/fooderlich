5d810b04a1c6e322ff01412d25d3ac98ecf6a6a5
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-23 12:07:55 +0300

    Expt friend_post_list_view.dart to components.dart

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -5,4 +5,5 @@ export 'card3.dart';
 export 'circle_image.dart';
 export 'today_recipe_list_view.dart';
 export 'friend_post_tile.dart';
+export 'friend_post_list_view.dart';
 export 'recipe_thumbnail.dart';
