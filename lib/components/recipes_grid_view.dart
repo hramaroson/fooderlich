b8b6b077c789205a9926c25763d36a414ae7b22f
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-23 12:13:49 +0300

    Add recipes_grid_view.dart

---
+++ lib/components/recipes_grid_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+
+class RecipesGridView extends StatelessWidget{
+  final List<SimpleRecipe> recipes;
+
+  const RecipesGridView({
+    super.key,
+    required this.recipes});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
