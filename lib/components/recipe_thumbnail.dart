0c7423a7d23088c72157976addaf4c70fd66050c
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-23 11:59:16 +0300

    Implement RecipeThumbnail

--- lib/components/recipe_thumbnail.dart
+++ lib/components/recipe_thumbnail.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
+import '../models/models.dart';
 
 class RecipeThumbnail extends StatelessWidget{
-  const RecipeThumbnail({super.key});
+  final SimpleRecipe recipe;
 
+  const RecipeThumbnail({
+      super.key,
+      required this.recipe,
+    });
+  
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                recipe.dishImage,
+                fit: BoxFit.cover),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text( 
+            recipe.title,
+            maxLines: 1,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          Text(
+            recipe.duration,
+            style: Theme.of(context).textTheme.bodyText1,
+          )
+        ],
+      ),
+
+    );
   }
 }
