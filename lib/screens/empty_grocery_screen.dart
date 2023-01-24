01a782f7c15d6b09db6eded151ec57ab6a2c1602
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-24 23:49:07 +0300

    Update empty_grocery_screen.dart

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -15,7 +15,23 @@ class EmptyGroceryscreen extends StatelessWidget{
               child: AspectRatio(
                 aspectRatio: 1 / 1,
                 child: Image.asset("assets/fooderlich_assets/empty_list.png"))
-            )
+            ),
+            Text(
+              "No Grocery",
+              style: Theme.of(context).textTheme.headline6),
+              const SizedBox(height: 16),
+              const Text(
+                "Shopping for ingredients?\n"
+                "Tap the + button to right them down",
+                textAlign: TextAlign.center),
+              MaterialButton(
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)
+                ),
+                color: Colors.green,
+                child: const Text("Browse recipes"),
+                onPressed: (){})
           ],
         ),
       )
