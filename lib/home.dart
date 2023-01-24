aef0f6e2e76ced3a62c987ce007b724dd125f91c
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-24 22:59:31 +0300

    Add GroceryScreen to Home widget

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/explore_screen.dart';
 import 'screens/recipes_screen.dart';
-import "components/card3.dart";
+import 'screens/grocery_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key, required this.title});
@@ -17,7 +17,7 @@ class HomeState extends State<Home> {
   static List<Widget> pages = [
     ExploreScreen(),
     RecipesScreen(),
-    const Card3()
+    const GroceryScreen()
   ];
 
   void _onItemTapped(int index){
