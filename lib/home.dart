8a13e424445175d5b5c4a4396b8cb0312c0c9038
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-26 21:22:51 +0300

    Add Consumer<TabManager> in build(context)

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'models/models.dart';
+
 import 'screens/explore_screen.dart';
 import 'screens/recipes_screen.dart';
 import 'screens/grocery_screen.dart';
@@ -12,45 +16,45 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  int _selectedItemIndex = 0;
-
+ 
   static List<Widget> pages = [
     ExploreScreen(),
     RecipesScreen(),
     const GroceryScreen()
   ];
 
-  void _onItemTapped(int index){
-    setState(() {
-        _selectedItemIndex = index;
-    });
-  }
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: pages[_selectedItemIndex],
+    return Consumer<TabManager>(
+      builder: (context, tabManager, child){
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: pages[tabManager.selectedTab],
 
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            label: 'Explore'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'Recipes'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'To Buy'
-          ),
-        ],
-        onTap: _onItemTapped,
-        currentIndex: _selectedItemIndex,
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.explore),
+                label: 'Explore'
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Recipes'
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: 'To Buy'
+              ),
+            ],
+            onTap: (index) {
+              tabManager.goToTab(index);
+            },
+            currentIndex: tabManager.selectedTab,
       ),
+    );
+      }
     );
   }
 }
