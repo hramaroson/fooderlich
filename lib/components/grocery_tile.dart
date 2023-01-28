5fd1ed92cd47338988b6d87df5a31af3405839e9
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-29 00:50:54 +0300

    Implement buildImportance() in GroceryTile

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -42,6 +42,8 @@ class GroceryTile extends StatelessWidget {
                   ),
                   const SizedBox(height: 4.0),
                   buildDate(),
+                  const SizedBox(height: 4.0),
+                  buildImportance()
               ])
             ],
           )
@@ -57,5 +59,31 @@ class GroceryTile extends StatelessWidget {
       dateString,
       style: TextStyle(decoration: textDecoration),
     );
+  }
+
+  Widget buildImportance(){
+    if(item.importance == Importance.low){
+      return Text(
+        'Low',
+        style: GoogleFonts.lato(decoration: textDecoration),
+      );
+    } else if (item.importance == Importance.medium) {
+      return Text(
+        'Medium',
+        style: GoogleFonts.lato(
+          fontWeight: FontWeight.w800,
+          decoration: textDecoration),
+      );
+    } else if (item.importance == Importance.high) {
+      return Text(
+        'High',
+        style: GoogleFonts.lato(
+          color: Colors.red,
+          fontWeight: FontWeight.w900,
+          decoration: textDecoration),
+      );
+    } else {
+      throw Exception('This importance type does not exist');
+    }
   }
 }
