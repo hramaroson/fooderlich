09a38db5c2ef35944bc76814d4469b0a650141bb
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-28 23:48:45 +0300

    Implement buildDate() in GroceryTile

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 import '../models/grocery_item.dart';
 
 class GroceryTile extends StatelessWidget {
   final GroceryItem item;
 
-  const GroceryTile({
+  final Function(bool?)? onComplete;
+  final TextDecoration textDecoration;
+
+  GroceryTile({
     super.key,
-    required this.item
-  });
+    required this.item,
+    this.onComplete
+  }): textDecoration = item.isComplete? TextDecoration.lineThrough : 
+  TextDecoration.none;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 100.0,
-      color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(width: 5.0, color: item.color),
+              const SizedBox(width: 16.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text( 
+                    item.name,
+                    style: GoogleFonts.lato(
+                      decoration: textDecoration,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  const SizedBox(height: 4.0),
+                  buildDate(),
+              ])
+            ],
+          )
+        ],
+      ),
+    );
+  }
+  
+  Widget buildDate(){
+    final dateFormatter = DateFormat('MMMM dd hh:mm a'); 
+    final dateString = dateFormatter.format(item.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
     );
   }
 }
