459026fc899596aab0f24710ac51439b431ec178
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-29 16:39:08 +0300

    Add Inkwell to GroceryListScreen to handle Gest.

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:fooderlich/components/grocery_tile.dart';
+
+import '../components/grocery_tile.dart';
+import '../screens/grocery_item_screen.dart';
 import '../models/models.dart';
 
 class GroceryListScreen extends StatelessWidget{
@@ -21,20 +23,36 @@ class GroceryListScreen extends StatelessWidget{
         itemCount: groceryItems.length,
         itemBuilder: (context, index){
           final item = groceryItems[index];
-          return GroceryTile(
-            item: item,
+          return InkWell(
+            child: GroceryTile(
+              item: item,
 
-            onComplete: (change){
-              if(change != null){
-                manager.completeItem(index, change);
+              onComplete: (change){
+                if(change != null){
+                  manager.completeItem(index, change);
+                }
               }
-            },
+            ),
+            onTap: (){
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => GroceryItemScreen(
+                    originalItem: item,
+                    onCreate: (item){}, 
+                    onUpdate: (item){
+                      manager.updateItem(item, index);
+                      Navigator.pop(context);
+                    })
+                )
+              );
+            }
           );
         }, 
         separatorBuilder:  (context, index){
           return const SizedBox(height: 16.0);
-        }, 
-       ),
+        }
+      ),
     );
   }
 }
